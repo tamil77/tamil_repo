c56c2139abee8c4ab29dfe6adc436df8ad826741
Tamilarasan Lakshmanan <tamilarasan.lakshmanan@multco.us>
2017-03-14 23:14:06 -0700

    Boolean

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
-//    var tapCount = 0
- 
+    //    var tapCount = 0
+    
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
@@ -20,21 +20,39 @@ class ViewController: UIViewController {
         theLabel.text = "Hello World"
     }
     @IBAction func theButton(_ sender: Any) {
- //       theLabel.text = "Hello There!!!"
- //       tapCount = tapCount + 1
- /*       if tapCount <= 6 {
-            print("Push Me Button Tapped")
-        }
-        else{
-            print("Push Me Button Tapped more than 5 times")
-        }
- */
-//        print(tapCount)
-//        print(text1)
+        //       theLabel.text = "Hello There!!!"
+        //       tapCount = tapCount + 1
+        /*       if tapCount <= 6 {
+         print("Push Me Button Tapped")
+         }
+         else{
+         print("Push Me Button Tapped more than 5 times")
+         }
+         */
+        //        print(tapCount)
+        //        print(text1)
         
-//        theLabel.text = String(Int(text1.text!)! + Int(text2.text!)!)
-// Or
-        theLabel.text = "Answer..\(Int(text1.text!)! + Int(text2.text!)!)"
+        //        theLabel.text = String(Int(text1.text!)! + Int(text2.text!)!)
+        // Or
+        var addition:Bool = true
+        
+        if Int(text1.text!)! > 8
+        {
+            addition = false
+        }
+        else
+        {
+            addition = true
+        }
+        
+        if addition
+        {
+            theLabel.text = "Answer..\(Int(text1.text!)! + Int(text2.text!)!)"
+        }
+        else
+        {
+            theLabel.text = "Answer..\(Int(text1.text!)! - Int(text2.text!)!)"
+        }
         
         print(text1.text!)
         print(text2.text!)
@@ -44,12 +62,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
